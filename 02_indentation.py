93ef56b788226f11abe3e10a4e2ba8d775d8373a
chiliai <m.hosseinzadeh.ai@gmail.com>
2022-07-29 21:17:58 +0430

    02

---
+++ 02_indentation.py
@@ -0,0 +1,7 @@
+# Indentation Tab
+if 5 > 2 :
+	print("five is greater than two!")
+
+# Indentation space
+if 5 > 2:
+ print("Five is greater than two!")
---
+++ 04_comments.py
@@ -0,0 +1,13 @@
+# this is a one line comment
+print("hey guys, i'm checking one line comment\n")
+
+print("hey guys, i'm checking end of a line comment\n") # end of a line cooment
+
+
+"""
+this is a comment
+written in 
+more than just one line
+"""
+print("hey guys, i'm chcking multi line cooment")
+
