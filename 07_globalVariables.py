a61a844dc8eef9b348a10dd1abda234e80e04e5a
chiliai <m.hosseinzadeh.ai@gmail.com>
2022-07-30 00:30:51 +0430

    variables

---
+++ 07_globalVariables.py
@@ -0,0 +1,51 @@
+
+ # way one
+word = "awesome"
+
+def myFunc():
+ print("python is " + word)
+ 
+myFunc()
+
+
+
+"""
+# way two
+x = "awesome"
+
+def myFunc():
+ x = "fantastic"
+ print("python is " + x)
+ 
+myFunc()
+
+print("python is "+x)
+"""
+
+
+
+"""
+#way three
+#x = "awesome" 
+def myFunc():
+ global x
+ x = "fantastic"
+
+#x = "awesome" 
+myFunc()
+#x = "awesome" 
+
+print("python is " + x)
+"""
+
+
+
+
+
+
+
+
+
+
+
+
