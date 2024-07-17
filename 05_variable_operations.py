3f39ec844ac5d4471c336a3d41a0e65b9d1bac28
notebook-ai <m.hosseinzadeh.ai@gmail.com>
2024-07-17 06:04:53 +0330

    change file name

---
+++ 05_variable_operations.py
@@ -0,0 +1,36 @@
+print("\n--------- create variables")
+a = 14
+b = "john"
+print("a is:", a)
+print("b is:", b)
+
+
+print("\n--------- create variables use many models")
+
+print("camel cases is 'myVariableName'")
+myVariableName = "John" # Camel Case
+
+print("pascal Case is 'MyVariableName'")
+MyVariableName = "John" # pascal Case
+
+print("Snake Case is 'my_variable_name'")
+my_variable_name = "John" # Snake Case
+
+
+
+print("\n--------- casting variables")
+num = 3
+print("num {} type is :".format(num), type(num))
+num = str(3)
+print("num {} type is :".format(num), type(num))
+"""
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+"""
+
+print("\n--------- case sensitive variables")
+x = 6
+X = 8
+print("x is:", x)
+print("X is:", X)
